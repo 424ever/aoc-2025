e1bc75517e39b16e7e2de60fcd5d694e44088f4b
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2025-12-06 19:24:36 +0100

    day6: part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,11 +1,13 @@
 use std::fs::read_to_string;
 
 use aoc_2025::surrounded;
+use itertools::Itertools;
 use winnow::{
-    ModalResult, Parser, Result,
+    Parser, Result,
     ascii::{dec_uint, line_ending, space0, space1},
     combinator::{alt, opt, repeat, separated, terminated},
     error::{StrContext, StrContextValue},
+    stream::AsChar,
 };
 
 #[derive(Debug, PartialEq, Eq)]
@@ -27,12 +29,12 @@ enum Op {
 
 fn main() {
     let i = read_to_string("input/day6").unwrap();
-    let i = parse.parse(&i).unwrap();
 
-    println!("part 1: {}", part_1(&i));
+    println!("part 1: {}", sum_solve(&parse_1.parse(&i).unwrap()));
+    println!("part 2: {}", sum_solve(&parse_2(&i)));
 }
 
-fn part_1(i: &Input) -> u64 {
+fn sum_solve(i: &Input) -> u64 {
     i.problems.iter().map(solve).sum()
 }
 
@@ -43,7 +45,7 @@ fn solve(p: &Problem) -> u64 {
     }
 }
 
-fn parse(i: &mut &str) -> Result<Input> {
+fn parse_1(i: &mut &str) -> Result<Input> {
     let ns: Vec<_> = repeat(1.., terminated(number_line, line_ending)).parse_next(i)?;
     let ops = terminated(op_line, opt(line_ending)).parse_next(i)?;
 
@@ -61,6 +63,52 @@ fn parse(i: &mut &str) -> Result<Input> {
     }
 
     Ok(r)
+}
+
+fn parse_2(i: &str) -> Input {
+    let nlines = i.lines().count();
+    let linelen = i.lines().next().unwrap().chars().count();
+
+    assert!(i.lines().map(|l| l.len()).all_equal());
+
+    let cols = (0..linelen).map(|n| i.lines().map(move |l| l.chars().nth(n).unwrap()));
+
+    let chunks = cols.rev().chunk_by(|key| !key.clone().all(|c| c == ' '));
+    let chunks = chunks
+        .into_iter()
+        .filter_map(|(key, group)| key.then_some(group));
+
+    let problems = chunks.map(|group| {
+        let (g1, g2) = group.tee();
+
+        (
+            g1.take(nlines - 1).map(|col| {
+                let (col, ass) = col.tee();
+                assert_eq!(ass.count(), nlines);
+
+                col.take(nlines - 1)
+                    .filter_map(|c| c.to_digit(10).map(u64::from))
+                    .fold(0, |acc, x| acc * 10 + x)
+            }),
+            g2.map(|c| c.last().unwrap())
+                .filter(|c| !c.is_space())
+                .next()
+                .unwrap(),
+        )
+    });
+
+    Input {
+        problems: problems
+            .map(|(nums, op)| Problem {
+                numbers: nums.collect(),
+                op: match op {
+                    '+' => Op::Plus,
+                    '*' => Op::Times,
+                    _ => unreachable!(),
+                },
+            })
+            .collect(),
+    }
 }
 
 fn number_line(i: &mut &str) -> Result<Vec<u64>> {
@@ -102,10 +150,9 @@ mod tests {
     );
 
     #[test]
-    fn test_parse() {
-        let p = parse.parse(INPUT);
+    fn test_parse_1() {
         assert_eq!(
-            p,
+            parse_1.parse(INPUT),
             Ok(Input {
                 problems: vec![
                     Problem {
@@ -130,6 +177,33 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_2() {
+        assert_eq!(
+            parse_2(INPUT),
+            Input {
+                problems: vec![
+                    Problem {
+                        numbers: vec![4, 431, 623],
+                        op: Op::Plus
+                    },
+                    Problem {
+                        numbers: vec![175, 581, 32],
+                        op: Op::Times
+                    },
+                    Problem {
+                        numbers: vec![8, 248, 369],
+                        op: Op::Plus
+                    },
+                    Problem {
+                        numbers: vec![356, 24, 1],
+                        op: Op::Times
+                    },
+                ]
+            }
+        );
+    }
+
+    #[test]
     fn test_parse_number_line() {
         assert_eq!(
             number_line.parse(" 45 64  387 23 "),
@@ -139,6 +213,6 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(&parse.parse(INPUT).unwrap()), 4277556);
+        assert_eq!(sum_solve(&parse_1.parse(INPUT).unwrap()), 4277556);
     }
 }
